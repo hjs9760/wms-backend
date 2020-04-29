fbb3c13401315dce3583f4b09561fef57229175b
hoxx <haru9760@naver.com>
2020-04-29 16:42:01 +0900

    - JPA Auditing 어노테이션 제거

--- src/main/java/com/ms/wms/WmsApplication.java
+++ src/main/java/com/ms/wms/WmsApplication.java
@@ -2,10 +2,7 @@ package com.ms.wms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-// JPA Auditing 활성화
-@EnableJpaAuditing
 @SpringBootApplication
 public class WmsApplication {
 
